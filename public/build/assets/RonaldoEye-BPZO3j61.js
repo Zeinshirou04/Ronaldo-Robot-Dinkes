208c82cce14076a5b690ef402432c822a2c40557
Zen <Farras.FF6@gmail.com>
2024-07-05 16:27:39 +0700

    Commit Build

---
+++ public/build/assets/RonaldoEye-BPZO3j61.js
@@ -0,0 +1,9 @@
+import{j as e}from"./app-CPV_-bDC.js";function t(){return e.jsx("section",{id:"display",className:"w-svw h-svh flex flex-row justify-center items-center",children:e.jsxs("div",{id:"baseEye",style:{maxWidth:"800px",maxHeight:"480px"},className:"w-full h-full flex flex-row justify-center items-center gap-52 bg-black",children:[e.jsx("style",{children:`
+                        @keyframes idle-eye {
+                            0%, 10% { transform: translate(-50%, -50%); }    /* Center */
+                            20%, 30% { transform: translate(-70%, -50%); }  /* Look left */
+                            40%, 50% { transform: translate(-50%, -50%); }  /* Center */
+                            60%, 70% { transform: translate(-30%, -50%); }  /* Look right */
+                            80%, 100% { transform: translate(-50%, -50%); } /* Center and delay */
+                        }
+                    `}),e.jsx("div",{id:"eyes-left",className:"w-1/4 h-3/5 bg-white relative",style:{borderRadius:"70%"},children:e.jsx("div",{id:"pupil-left",className:"w-1/2 h-3/5 bg-black absolute",style:{top:"50%",transform:"translateY(-50%)",left:"50%",borderRadius:"70%",animationName:"idle-eye",animationDuration:"20s",animationIterationCount:"infinite",animationTimingFunction:"ease-in-out"},children:e.jsx("div",{className:"w-12 h-16 bg-gray-200/30",style:{borderRadius:"60%"}})})}),e.jsx("div",{id:"eyes-right",className:"w-1/4 h-3/5 bg-white relative",style:{borderRadius:"70%"},children:e.jsx("div",{id:"pupil-right",className:"w-1/2 h-3/5 bg-black absolute",style:{top:"50%",transform:"translateY(-50%)",left:"50%",borderRadius:"70%",animationName:"idle-eye",animationDuration:"20s",animationIterationCount:"infinite",animationTimingFunction:"ease-in-out"},children:e.jsx("div",{className:"w-12 h-16 bg-gray-200/30",style:{borderRadius:"60%"}})})})]})})}export{t as default};
